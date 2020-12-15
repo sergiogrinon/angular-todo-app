175553767b52c71e425445dd07d623728ae7aa9b
Sergio Grinon Chico <sergio.grinon.chico.st@everis.com>
2020-12-15 11:50:50 +0100

    Main project working
    Signed-off-by: Sergio Grinon Chico <sergio.grinon.chico.st@everis.com>

--- angular-todo-app/src/app/app.component.ts
+++ angular-todo-app/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todoArray = [];
   title = 'angular-todo-app';
+
+  /*add task*/  
+  addTodo(value : String) {
+    this.todoArray.push(value);
+  }
+
+  /*delete task*/  
+  deleteItem(todo){   
+    for(let i=0 ;i<= this.todoArray.length ;i++){    
+      if(todo== this.todoArray[i]){     
+        this.todoArray.splice(i,1)    
+      }   
+    }  
+  }
+
 }
